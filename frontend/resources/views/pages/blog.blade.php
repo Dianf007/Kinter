f4e0944131144e7b8c9f617f21586a5ed6d58990
Dianf007 <fnurwahid007@gmail.com>
2025-07-18 00:05:56 +0700

    add blog functionality with BlogController and blog view

---
+++ frontend/resources/views/pages/blog.blade.php
@@ -0,0 +1,289 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <!-- blog start -->
+        <section class="blog-content-area pt-120 pb-90">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8">
+                <div class="blog-wrapper mb-30">
+                  <article class="post-item format-standard mb-30">
+                    <div class="post-thumb">
+                      <a href="blog-details.html"
+                        ><img src="assets/img/blog/blog01.jpg" alt="blog"
+                      /></a>
+                    </div>
+                    <div class="post-content">
+                      <div class="post-meta mb-20">
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-calendar-alt"></i> 27 Juli
+                            2025</a
+                          ></span
+                        >
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-user"></i> Admin PK</a
+                          ></span
+                        >
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-comments"></i> 24 Comments</a
+                          ></span
+                        >
+                      </div>
+                      <h4 class="post-title">
+                        <a href="blog-details.html"
+                          >Planting Seeds in the Hearts of Preschoolers</a
+                        >
+                      </h4>
+                      <div class="post-text">
+                        <p>
+                          Completely deploy focused supply chains rather than
+                          viraling an e-services productivate pandemc
+                          experiences via interoperable niche markets.
+                          Dramatically incentivize superior functionalities with
+                          Rapidiously evolve impactful core competencies without
+                          web-enabled vortals
+                        </p>
+                      </div>
+                      <div class="read-more mt-20">
+                        <a class="thm-btn" href="blog-details.html"
+                          >Read More</a
+                        >
+                      </div>
+                    </div>
+                  </article>
+                  <article class="post-item format-gallery mb-30">
+                    <div class="gallery-post-active owl-carousel">
+                      <div class="post-thumb">
+                        <img src="assets/img/blog/blog02.jpg" alt="" />
+                      </div>
+                      <div class="post-thumb">
+                        <img src="assets/img/blog/blog03.jpg" alt="" />
+                      </div>
+                      <div class="post-thumb">
+                        <img src="assets/img/blog/blog01.jpg" alt="" />
+                      </div>
+                    </div>
+                    <div class="post-content">
+                      <div class="post-meta mb-20">
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-calendar-alt"></i> 27 Juli
+                            2025</a
+                          ></span
+                        >
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-user"></i> Admin PK</a
+                          ></span
+                        >
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-comments"></i> 24 Comments</a
+                          ></span
+                        >
+                      </div>
+                      <h4 class="post-title">
+                        <a href="blog-details.html"
+                          >Why children need a Healthy Environment</a
+                        >
+                      </h4>
+                      <div class="post-text">
+                        <p>
+                          Completely deploy focused supply chains rather than
+                          viraling an e-services productivate pandemc
+                          experiences via interoperable niche markets.
+                          Dramatically incentivize superior functionalities with
+                          Rapidiously evolve impactful core competencies without
+                          web-enabled vortals
+                        </p>
+                      </div>
+                      <div class="read-more mt-20">
+                        <a class="thm-btn" href="blog-details.html"
+                          >Read More</a
+                        >
+                      </div>
+                    </div>
+                  </article>
+                  <article class="post-item format-video mb-30">
+                    <div class="post-thumb post-video">
+                      <img src="assets/img/blog/blog03.jpg" alt="blog" />
+                      <a
+                        class="popup-video video-icon"
+                        href="https://www.youtube.com/watch?v=RHgbJr4pf1A"
+                        ><i class="far fa-play"></i
+                      ></a>
+                    </div>
+                    <div class="post-content">
+                      <div class="post-meta mb-20">
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-calendar-alt"></i> 27 Juli
+                            2025</a
+                          ></span
+                        >
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-user"></i> Admin PK</a
+                          ></span
+                        >
+                        <span
+                          ><a href="#!"
+                            ><i class="far fa-comments"></i> 24 Comments</a
+                          ></span
+                        >
+                      </div>
+                      <h4 class="post-title">
+                        <a href="blog-details.html"
+                          >Full-day kindergarten in Alberta another.</a
+                        >
+                      </h4>
+                      <div class="post-text">
+                        <p>
+                          Completely deploy focused supply chains rather than
+                          viraling an e-services productivate pandemc
+                          experiences via interoperable niche markets.
+                          Dramatically incentivize superior functionalities with
+                          Rapidiously evolve impactful core competencies without
+                          web-enabled vortals
+                        </p>
+                      </div>
+                      <div class="read-more mt-20">
+                        <a class="thm-btn" href="blog-details.html"
+                          >Read More</a
+                        >
+                      </div>
+                    </div>
+                  </article>
+                  <div class="pagination pt-20 mb-30">
+                    <ul>
+                      <li>
+                        <a href="#!"
+                          ><i class="far fa-angle-double-left"></i
+                        ></a>
+                      </li>
+                      <li class="active"><a href="#!">1</a></li>
+                      <li><a href="#!">2</a></li>
+                      <li><a href="#!">3</a></li>
+                      <li>
+                        <a href="#!"
+                          ><i class="far fa-angle-double-right"></i
+                        ></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="blog-sidebar">
+                  <div class="widget mb-30">
+                    <div class="search-form">
+                      <input type="text" placeholder="Search Here..." />
+                      <button type="submit">
+                        <i class="far fa-search"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="widget mb-30">
+                    <h2 class="widget-header">Post Category</h2>
+                    <ul class="widget-wrapper">
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>PHP</span>
+                          <span>12</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>Laravel</span>
+                          <span>8</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>JavaScript</span>
+                          <span>15</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>VueJS</span>
+                          <span>5</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>ReactJS</span>
+                          <span>4</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>HTML & CSS</span>
+                          <span>20</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a class="d-flex justify-content-between" href="#!">
+                          <span><i class="far fa-angle-double-right"></i>Database</span>
+                          <span>7</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="widget mb-30">
+                    <h2 class="widget-header">Most Popular Posts</h2>
+                    <ul class="widget-wrapper widget-post">
+                      <li class="d-flex flex-wrap justify-content-between">
+                        <div class="widget-post-thumb">
+                          <a href="blog-details.html"><img src="assets/img/blog/blog-widget/widget-post01.jpg" alt="blog"></a>
+                        </div>
+                        <div class="widget-post-content">
+                          <h5><a href="blog-details.html">Belajar Dasar PHP untuk Pemula</a></h5>
+                          <span>10 Juni 2025</span>
+                        </div>
+                      </li>
+                      <li class="d-flex flex-wrap justify-content-between">
+                        <div class="widget-post-thumb">
+                          <a href="blog-details.html"><img src="assets/img/blog/blog-widget/widget-post02.jpg" alt="blog"></a>
+                        </div>
+                        <div class="widget-post-content">
+                          <h5><a href="blog-details.html">Menguasai Framework Laravel</a></h5>
+                          <span>5 Mei 2025</span>
+                        </div>
+                      </li>
+                      <li class="d-flex flex-wrap justify-content-between">
+                        <div class="widget-post-thumb">
+                          <a href="blog-details.html"><img src="assets/img/blog/blog-widget/widget-post03.jpg" alt="blog"></a>
+                        </div>
+                        <div class="widget-post-content">
+                          <h5><a href="blog-details.html">Tips JavaScript Modern</a></h5>
+                          <span>20 April 2025</span>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="widget mb-30">
+                    <h2 class="widget-header">Popular Tags</h2>
+                    <div class="tagcloud">
+                      <a class="sidebar-tag" href="#!">PHP</a>
+                      <a class="sidebar-tag" href="#!">Laravel</a>
+                      <a class="sidebar-tag" href="#!">JavaScript</a>
+                      <a class="sidebar-tag" href="#!">VueJS</a>
+                      <a class="sidebar-tag" href="#!">ReactJS</a>
+                      <a class="sidebar-tag" href="#!">CSS</a>
+                      <a class="sidebar-tag" href="#!">HTML</a>
+                      <a class="sidebar-tag" href="#!">MySQL</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    <!-- blog end -->
+@endsection
